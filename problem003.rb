15a1dc5d67cf77ed104893700a7d1a7373420e42
MyMac <damian.nicholson21@googlemail.com>
2008-12-04 16:13:27 +0000

    Refined solution to problem003.rb

--- problem003.rb
+++ problem003.rb
@@ -1,10 +1,10 @@
 def sieve(max)
   sieve = []
-  for i in 2 .. max
+  for i in 2..max
     sieve[i] = i
   end
 
-  for i in 2 .. Math.sqrt(max)
+  for i in 2..Math.sqrt(max)
     next unless sieve[i]
     (i*i).step(max, i) do |j|
       sieve[j] = nil
@@ -14,14 +14,7 @@ def sieve(max)
 end
 
 def problem3(n)
-  factors = Array.new
-  primes = sieve(Math.sqrt(n).to_i)#.inject(0) {|result, p| result = p if (p%n).zero?}
-  
-  for p in primes
-    factors << p if (n%p).zero?
-  end
-  factors.last
+  sieve(Math.sqrt(n).to_i).select {|p| (n%p).zero?}.last
 end
-
 
 puts "Answer = #{problem3(600851475143)}"
